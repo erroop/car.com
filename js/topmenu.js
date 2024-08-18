febbbc4e549f6a446a1b3e3214560bef002322a5
erroop <erroop0@gmail.com>
2024-08-18 18:33:48 +0900

    responsible_main_commit

--- js/topmenu.js
+++ js/topmenu.js
@@ -1,13 +1,20 @@
 $(document).ready(function(){
-    let count = 0;
-    $(".nav_list").mouseenter(function(){
-        $("#hd_bg").css({height:"280px"})
-        $(".hidden_nav").css({opacity:"1"})
-        count = 1;
-    });
-    $(".nav_list").mouseleave(function(){
-        $("#hd_bg").css({height:"0px"})
-        $(".hidden_nav").css({opacity:"0"})
-        count = 0;
-    });
-})
+    /* 반응형 [s] */
+    $("#m_navBtn").click(function(){
+        m++;
+        if(m%2 == 1){
+            $("#m_navBtn").addClass("on");
+            $("#navWrap").fadeIn(300).addClass("on");
+        }else{
+            m_navClose(); 
+        }; 
+    });	
+;	
+    m = 0;  	
+    function m_navClose() { 
+        $("#m_navBtn").removeClass("on");
+        $("#navWrap").fadeOut(300).removeClass("on");	
+    }	
+    /* 반응형 [e] */
+}
+)
